e5abc6fd1d1483005ff380aa3058b139433ab10a
Dong <2829139244@qq.com>
2020-01-08 19:43:37 +0800

    progect init

---
+++ VUE/my-project/src/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input v-model="inputValue"/>
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <todoitem v-for="(item, index) of list" :key="index" :content="item" :index="index" @deleteItem="handleDelete">
+      </todoitem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoitem from './components/TodoItem'
+export default {
+  components: {
+    todoitem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: ['1', '2', '3']
+    }
+  },
+  methods: {
+    handleSubmit () {
+      if (this.inputValue !== '') {
+        this.list.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/my-project/src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li @click="handleDelete" class="item">
+    {{content}}
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    handleDelete () {
+      this.$emit('deleteItem', this.index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item{
+  color: green
+}
+</style>
